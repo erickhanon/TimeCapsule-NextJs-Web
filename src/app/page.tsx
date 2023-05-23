230137d7176ebff53bbaedcba5c21d25fb4dd503
erickhanon <erick.hanon@gmail.com>
2023-05-23 16:42:26 -0300

    home finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,10 +22,33 @@ export default function Home() {
             memórias.
           </p>
         </a>
-
         {/* Hero section */}
-        <div>
+        <div className="space-y-5">
           <Image src={logo} alt="Logo" />
+          <div className="max-w[420px] space-y-1 ">
+            <h1 className="mt-5 text-5xl font-bold leading-tight text-gray-50 ">
+              Sua Capsula do Tempo
+            </h1>
+            <p className="text-lg leading-relaxed">
+              Guarde todos os momentos os importantes da sua vida!{' '}
+            </p>
+          </div>
+          <a
+            href=""
+            className="inline-block rounded-full bg-green-600 px-5 py-3 font-alt text-sm uppercase leading-none text-gray-50 transition-colors hover:bg-green-800 "
+          >
+            Guarde Uma Lembrança
+          </a>
+        </div>
+        <div className="text-sm leading-relaxed text-gray-200">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="underline hover:text-gray-100"
+            href="github.com/erickhanon"
+          >
+            Feito Com ❤ por Erick Marques!
+          </a>
         </div>
       </div>
       {/* Right side */}
